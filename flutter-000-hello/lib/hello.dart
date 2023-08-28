8b7b3a193f1dd04d3f1beb69edaa7d44b6ba7633
2gechan <7532602@gmail.com>
2023-08-28 15:46:54 +0900

    안드로이드

---
+++ flutter-000-hello/lib/hello.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const Center(
+      child: NewWidget(),
+    ),
+  );
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "반갑습니다",
+      style: TextStyle(
+        fontSize: 50,
+        color: Color.fromARGB(255, 0, 255, 1),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+  }
+}
