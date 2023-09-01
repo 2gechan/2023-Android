141857174d29a17253f34f2f0bc6ca9e26c86690
2gechan <7532602@gmail.com>
2023-09-01 14:26:56 +0900

    todolist

--- flutter-006-todolist/lib/main.dart
+++ flutter-006-todolist/lib/main.dart
@@ -27,20 +27,29 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPage extends State<StartPage> {
-  var todoList = [
-    Todo(
-      sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
-      stime: DateFormat("HH:ss:mm").format(DateTime.now()),
-      content: "오늘은 금요일",
-      complete: false,
-    ),
-    Todo(
-      sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
-      stime: DateFormat("HH:ss:mm").format(DateTime.now()),
-      content: "Flutter 프로젝트",
-      complete: false,
-    )
-  ];
+  var todoList = [];
+
+//   var todoList = [
+//     Todo(
+//       sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
+//       stime: DateFormat("HH:ss:mm").format(DateTime.now()),
+//       content: "오늘은 금요일",
+//       complete: false,
+//     ),
+//     Todo(
+//       sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
+//       stime: DateFormat("HH:ss:mm").format(DateTime.now()),
+//       content: "Flutter 프로젝트",
+//       complete: false,
+//     )
+//   ];
+
+  Todo getTodo(String content) => Todo(
+        sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
+        stime: DateFormat("HH:mm:ss").format(DateTime.now()),
+        content: content,
+        complete: false,
+      );
 
   Widget alertDialog(BuildContext context) => AlertDialog(
         actions: [
@@ -48,8 +57,16 @@ class _StartPage extends State<StartPage> {
             // keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.go,
             onSubmitted: (value) {
-              var snackBar = SnackBar(content: Text(value));
+              var snackBar = SnackBar(
+                content: Text("$value가 추가되었음"),
+              );
               ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+              var todo = getTodo(value);
+              Navigator.of(context).pop();
+              setState(() {
+                todoList.add(todo);
+              });
             },
             decoration: const InputDecoration(
               hintText: "할일을 입력해주세요",
@@ -59,6 +76,14 @@ class _StartPage extends State<StartPage> {
       );
   @override
   Widget build(BuildContext context) {
+    // state 클래스 내의 함수를 실행하여 결과를 가져온 후 todolist를 만든다.
+    // 이때 클래스 내의 함수를 호출하여 다른 변수를 만드는 경우
+    // 이 코드는 build 함수 내부에서 실행해야 한다.
+    // todoList.add(value) = [
+    //   getTodo("flutter project"),
+    //   getTodo("피곤한 하루"),
+    // ];
+
     return Scaffold(
       appBar: AppBar(
           leading: Image.asset(
@@ -80,28 +105,42 @@ class _StartPage extends State<StartPage> {
         itemCount: todoList.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(todoList[index].sdate),
-                      Text(todoList[index].stime),
-                    ],
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        todoList[index].content,
-                        style:
-                            const TextStyle(fontSize: 20, color: Colors.blue),
+            onTap: () {},
+            selectedColor:
+                const Color.fromARGB(98, 79, 179, 236).withOpacity(0.5),
+            splashColor:
+                const Color.fromARGB(255, 88, 105, 202).withOpacity(0.5),
+            title: Dismissible(
+              key: Key(todoList[index].content),
+              background: Container(color: Colors.green),
+              onDismissed: (direction) {
+                setState(() {
+                  todoList.removeAt(index);
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(todoList[index].sdate),
+                        Text(todoList[index].stime),
+                      ],
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          todoList[index].content,
+                          style:
+                              const TextStyle(fontSize: 20, color: Colors.blue),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
